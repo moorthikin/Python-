a1d0824bac7a8c6aad33c7e63e8d8c97dc0a490a
moorthi <velayuthamoorthi.l@guruits.com>
2025-02-19 18:38:46 +0530

    condtional statements

---
+++ If_else.py
@@ -0,0 +1,44 @@
+#if - If some condition is True do something
+#else - do something here
+
+
+#for check the user eligible to sign up in the application
+'''
+
+age = int(input("Enter your age : " ))
+if age >=100:
+    print("You are too old to sign up!")
+elif age >= 18 :
+    print("You are eligible to sign up!")
+elif age <= 0 :
+    print("You haven't born yet!")
+else :
+    print("You are not eligible to sign up!")
+'''
+
+#For ask the customer in hotel need food
+
+'''
+response = input("Do you need food [Type (Y/N)] : ")
+
+if response == "Y" :
+    print("We will reach in one minute :)")
+else :
+    print("Happy for contact us!")
+'''
+
+#For check the user entered the name is name field
+
+# name_field = input("Enter the name : ")
+#
+# if name_field == "":
+#     print("You haven't type anything in the field!")
+# else :
+#     print(f"Hello {name_field}")
+
+
+
+
+
+
+
---
+++ math.py
@@ -0,0 +1,21 @@
+import math
+x = 3.12
+y = -2
+z = 43
+
+#round() method convert the decimal number into whole number
+#result = round(x)
+
+#abs() method convert the negative integer to positive integer
+#result = abs(y)
+
+# The pow() function returns the value of x to the power of y (xy).
+#result = pow(4, 3)
+
+#max() function returns the maximum value in the given numbers
+#result = max(x,y,z)
+
+#min() method return the minimum number in the chosen numbers
+#result = min(x,y,z)
+print(math.pi)
+print(math.e)
