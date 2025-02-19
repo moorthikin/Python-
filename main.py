c32ed86ae4895788e54ac15b23430f7d10b21009
moorthi <velayuthamoorthi.l@guruits.com>
2025-02-19 10:50:40 +0530

    Data Types and Variables

---
+++ main.py
@@ -0,0 +1,4 @@
+#How to print something in python
+print("Hello Bro!")
+print("How are you?")
+print("I am well")
---
+++ variables.py
@@ -0,0 +1,17 @@
+#Variables are container to store the ("String", int, float, bool)
+#Data Types
+#1.Strings -
+student_name = "Jerome"
+#integer - whole numbers
+student_age = 12
+#float - numbers contain decimal values in the end
+gpa = 90.3
+#bool - boolean either returns true or false value
+is_studying = True
+#f"" - formatted string for interpolate the strings in the print statement
+print(f"Student Name is : {student_name}")
+print(f"Student Age is : {student_age}")
+print(f"Student Gpa is : {gpa}")
+print(f"Student is studying Right Now ? : {is_studying}")
+
+
